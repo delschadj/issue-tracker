e7430012be5e14104aaf11c3d023f919556a9cfb
delschadj <delschad.jankir@gmx.de>
2022-12-06 02:23:13 +0100

    Dark / Light Mode first setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { ColorModeContext, useMode } from "./theme";
+
 import Login from "./components/Authentication/Login";
 import Signup from './components/Authentication/Signup';
 import Account from './components/Authentication/Account';
@@ -14,39 +18,49 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
 function App() {
+  const [theme, colorMode] = useMode ();
   const [emailApp, setEmailApp] = useState ("")
 
-  return (
-    <div>
-      
-      <AuthContextProvider>
-        <Routes>
-          <Route path='/' element={<Login setEmailApp={setEmailApp} />} />
-          <Route  path='/signup' element={<Signup setEmailApp={setEmailApp} />} />
-          <Route  path="/welcome" element ={<Welcome emailApp={emailApp}/>}/>
+  return ( 
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+
+          <div className="app">
+            <main className="content">
+              <AuthContextProvider>
+                <Routes>
+                  <Route path='/' element={<Login setEmailApp={setEmailApp} />} />
+                  <Route  path='/signup' element={<Signup setEmailApp={setEmailApp} />} />
+                  <Route  path="/welcome" element ={<Welcome emailApp={emailApp}/>}/>
 
 
-          <Route path='/home' element={
-            <ProtectedRoute>
-              <Home />
-          </ProtectedRoute>}/>
+                  <Route path='/home' element={
+                    <ProtectedRoute>
+                      <Home />
+                  </ProtectedRoute>}/>
 
-          <Route path='/issues' element={
-            <ProtectedRoute>
-              <Issues />
-          </ProtectedRoute>}/>
+                  <Route path='/issues' element={
+                    <ProtectedRoute>
+                      <Issues />
+                  </ProtectedRoute>}/>
 
-          
+                  
 
-          <Route path='/account' element={ 
-          <ProtectedRoute> 
-            <Account /> 
-          </ProtectedRoute>}/>
-          
-          
-        </Routes>
-      </AuthContextProvider>
-    </div>
+                  <Route path='/account' element={ 
+                  <ProtectedRoute> 
+                    <Account /> 
+                  </ProtectedRoute>}/>
+                  
+                  
+                </Routes>
+              </AuthContextProvider>
+            </main>
+
+          </div>
+
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
